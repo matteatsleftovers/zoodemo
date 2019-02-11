8d90dff79100e86f1cec2f4dcf5335ec6053b6f1
Matt Lauer <matteatsleftovers@gmail.com>
2019-02-11 11:32:03 -0500

    Add Pens to Zoo, and print those Pens

--- src/Zoo.java
+++ src/Zoo.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Zoo {
+
+    private ArrayList<Pen> pens;
+
+    public Zoo() {
+        this.pens = new ArrayList<Pen>();
+    }
+
+    public void printAllAnimalsInZoo() {
+        System.out.println("Here are all your animals: ");
+        for (int i = 0; i < pens.size(); i++) {
+            pens.get(i).showAllAnimals();
+        }
+    }
+
 }
