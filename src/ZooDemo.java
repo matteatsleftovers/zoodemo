3b77cee06ea72aa66bfd87efd3cff361577e91e8
Matt Lauer <matteatsleftovers@gmail.com>
2019-02-11 11:30:21 -0500

    Add test code to confirm proper function of Pens and Animal/BabyAnimal

--- src/ZooDemo.java
+++ src/ZooDemo.java
@@ -1,6 +1,13 @@
 public class ZooDemo {
 
     public static void main(String[] args) {
-	// write your code here
+        Pen lions = new Pen("lions");
+        Animal lion = new Animal("lion", "large", "female");
+        lions.addAnimalToPen(lion);
+        lions.addBabyAnimalToPen(new BabyAnimal("lion", "small", "male", 1));
+        lions.showAllAnimals();
+        lions.removeAnimal(0);
+        lions.removeBabyAnimal(0);
+        lions.showAllAnimals();
     }
 }
