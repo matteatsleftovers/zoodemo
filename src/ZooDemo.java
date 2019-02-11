982be2a2df13e7b21309a8b26e7a9f951cd51b77
Matt Lauer <matteatsleftovers@gmail.com>
2019-02-11 13:32:50 -0500

    Add option for user to set up pen

--- src/ZooDemo.java
+++ src/ZooDemo.java
@@ -1,16 +1,42 @@
+import java.util.Scanner;
+
 public class ZooDemo {
 
     public static void main(String[] args) {
-        Pen lions = new Pen("lions");
-        Animal lion = new Animal("lion", "large", "female");
-        lions.addAnimalToPen(lion);
-        lions.addBabyAnimalToPen(new BabyAnimal("lion", "small", "male", 1));
-        lions.showAllAnimals();
 
+        boolean repeat;
+        Scanner scanner = new Scanner(System.in);
         Zoo zoo = new Zoo();
-        zoo.addPen(lions);
-        zoo.printAllPens();
-        zoo.removePen("lions");
-        zoo.printAllPens();
+
+        do {
+            System.out.println("Welcome to the zoo!");
+            System.out.println("What would you like to do?");
+            System.out.println("1. Set up pen");
+            System.out.println("2. Remove pen");
+            System.out.println("3. Add animal to pen");
+            System.out.println("4. Add baby animal to pen");
+            System.out.println("5. Remove animal from pen");
+            System.out.println("6. Remove baby animal from pen");
+            System.out.println("7. Display all animals in a pen");
+            System.out.println("8. Display all animals in the zoo");
+
+            int userInput = scanner.nextInt();
+            switch(userInput) {
+                case 1:
+                    System.out.println("What's the name of the pen?");
+                    scanner.nextLine();
+                    String penName = scanner.nextLine();
+                    Pen newPen = new Pen(penName);
+                    zoo.addPen(newPen);
+                    break;
+                default:
+                    System.out.println("That's not an option.");
+                    break;
+            }
+
+            System.out.println("\nWould you like to continue? y/N");
+            String continueInput = scanner.nextLine();
+            repeat = continueInput.equals("y");
+        } while(repeat);
     }
 }
