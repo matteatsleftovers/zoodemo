3735ddfaf8b6dcf29235fd835a58ecc3d7920714
Matt Lauer <matteatsleftovers@gmail.com>
2019-02-11 10:17:05 -0500

    Add property, getter/setter, and constructor for BabyAnimal

--- src/BabyAnimal.java
+++ src/BabyAnimal.java
@@ -1,2 +1,17 @@
 public class BabyAnimal extends Animal {
+
+    private int age;
+
+    public BabyAnimal(String species, String size, String gender, int age) {
+        super(species, size, gender);
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
